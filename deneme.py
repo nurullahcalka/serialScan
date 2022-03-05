91d154433ffd6ff90bed7c89c74a0346ce841e0e
nurullahcalka <calkanurullah47@gmail.com>
2022-03-05 18:01:00 +0300

    aa

--- deneme.py
+++
@@ -1,9 +0,0 @@
-import serial.tools.list_ports
-connected = []
-while True:
-    comlist = serial.tools.list_ports.comports()
-    for element in comlist:
-        connected.append(element.device)
-
-    print("Bağlı COM portları: " + str(connected))
-    connected.clear()
